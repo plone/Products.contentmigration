63ca13f222d7e331ad5b3d07dac6baa59fad2c40
Ross Patterson <me@rpatterson.net>
2019-02-28 12:47:04 -0800

    Attempt to fix plone.app.blob Jenkins errors, #23

--- Products/contentmigration/basemigrator/migrator.py
+++ Products/contentmigration/basemigrator/migrator.py
@@ -50,7 +50,6 @@ try:
     IRedirectionStorage  # pyflakes
 except ImportError:
     IRedirectionStorage = None
-from plone import api
 
 try:
     from Products.Archetypes.config import UUID_ATTR
@@ -629,7 +628,7 @@ class UIDMigrator(object):
             setattr(self.old, UUID_ATTR, None)
         if queryAdapter(self.new, IMutableUUID):
             IMutableUUID(self.new).set(str(uid))
-            uid_catalog = api.portal.get_tool('uid_catalog')
+            uid_catalog = getToolByName(self.parent, 'uid_catalog')
             uid_catalog._catalogObject(self.new, self.new.getPhysicalPath())
         else:
             self.new._setUID(uid)
